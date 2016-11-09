46466acdf3f04af8d9f363ea95fab75bd27a7041
FreaKzero <pete@freakzero.com>
2016-11-09 01:34:25 +0100

    First tests with new SettingsController

--- src/app/controllers/settingsController.js
+++ src/app/controllers/settingsController.js
@@ -1,7 +1,15 @@
 (function() {
   app.controller('settingsController', ['$scope', settingsController]);
 
-  function settingsController() {
-
+  function settingsController($scope) {
+    $scope.somelist = [
+      {
+        icon: 'someicon',
+        name: 'gzDoom',
+        path: 'path',
+        saves: 'path',
+        active: false
+      }
+    ];
   }
 })();
