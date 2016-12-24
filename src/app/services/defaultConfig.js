a7bf3d6a82b6d8bd9783f76b381f77ea11284a8d
FreaKzero <pete@freakzero.com>
2016-12-25 00:01:10 +0100

    Remove not needed configprops

--- src/app/services/defaultConfig.js
+++ src/app/services/defaultConfig.js
@@ -1,46 +1,13 @@
 (function() {
   app.constant('DEFAULTCONFIG', {
-    'active': {
-      'doom64ex': false,
-      'doomretro': false,
-      'doomrpg': false,
-      'doomrpgrl': false,
-      'doomrpgrlmonsters': false,
-      'gzdoom': false,
-      'oblige': false,
-      'prboomp': false,
-      'zandronum': false,
-      'zdoom': false
-    },
     'dontShowUpdate': '0.0.0',
-    'engines': {
-      'doom64ex': '',
-      'doomretro': '',
-      'doomrpg': '',
-      'doomrpgrl': '',
-      'gzdoom': '',
-      'prboomp': '',
-      'zandronum': '',
-      'zdoom': ''
-    },
     'freshinstall': true,
     'initList': false,
     'iwadpath': '',
-    'misc': {
-      'doom64exsound': '',
-      'doomrpg': {
-        'brightmaps': '',
-        'extras': '',
-        'rlarsenalpath': '',
-        'rlarsenalwad': '',
-        'rlhudwad': '',
-        'rlmonsterspath': '',
-        'rlmonsterswad': '',
-        'vanilla': ''
-      },
-      'preselectedengine': ''
-    },
+    'preselectedengine': '',
     'modlistpath': '',
+    'screenshotpath': '',
+    'wadpath': '',
     'oblige': {
       'binary': '',
       'configs': '',
@@ -48,17 +15,6 @@
     },
     'online': {
       'client': ''
-    },
-    'savepaths': {
-      'doom64ex': '',
-      'doomretro': '',
-      'doomrpg': '',
-      'gzdoom': '',
-      'prboomp': '',
-      'zandronum': '',
-      'zdoom': ''
-    },
-    'screenshotpath': '',
-    'wadpath': ''
+    }
   });
 })();
